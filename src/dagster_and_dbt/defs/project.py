efb127e50285191ff5bb0831497d97f0b7869a82
Michael <m_sozonov@outlook.com>
2026-04-07 21:01:48 +0300

    feat: create project

---
+++ src/dagster_and_dbt/defs/project.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from dagster_dbt import DbtProject
+
+
+dbt_project = DbtProject(
+    project_dir=Path(__file__).parent.parent.joinpath("analytics").resolve(),
+)
+print(f"Using dbt project at {dbt_project.project_dir}")
+dbt_project.prepare_if_dev()
