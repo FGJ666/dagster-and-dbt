4755b4cdb64246f68157ca2b10f34d4588162ba5
Michael <m_sozonov@outlook.com>
2026-04-07 23:16:34 +0300

    fix

--- dagster_and_dbt/project.py
+++ dagster_and_dbt/project.py
@@ -3,7 +3,7 @@ from dagster_dbt import DbtProject
 
 
 dbt_project = DbtProject(
-    project_dir=Path(__file__).parent.parent.joinpath("analytics").resolve(),
+    project_dir=Path(__file__).parent.joinpath("analytics").resolve(),
 )
 print(f"Using dbt project at {dbt_project.project_dir}")
 dbt_project.prepare_if_dev()
